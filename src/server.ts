510f747ba4e13cf86b18d1f2bc25e97b1a6e0dd9
Matteo Piazza <matteo.piazza@exmachina.ch>
2021-01-26 15:01:31 +0100

    cors

--- src/server.ts
+++ src/server.ts
@@ -17,10 +17,11 @@ import { IndexRouter } from './controllers/index.router';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // TODO: restrict CORS access
+  let ALLOWED_ORIGINS = ["http://ailuropodarts.wtf", "http://d5n930n193m31.cloudfront.net", "http://udagram-piazza-frontend.s3-website.us-east-2.amazonaws.com", "http://localhost:8100"];
   app.use(function(req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-    res.header("Access-Control-Allow-Origin", "*");
+    let origin = req.headers.origin;
+    let theOrigin = (ALLOWED_ORIGINS.indexOf(origin) >= 0) ? origin : ALLOWED_ORIGINS[0];
+    res.header("Access-Control-Allow-Origin", theOrigin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
